d632e4f9b13954a2995df9e8f1769c6e4b1024f4
Chinedu codePanda Ikechi <Neddymond10@gmail.com>
2020-03-28 23:59:20 +0100

    Initial Commit. Refilled the board after three or more dots match.

---
+++ Assets/Scripts/Board.cs
@@ -0,0 +1,206 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Board : MonoBehaviour
+{
+    public int width;
+    public int height;
+
+    //private BackgroundTile[,] allTiles;
+
+    public GameObject[] dots;
+    public GameObject[,] allDots;
+    public GameObject tilePrefab;
+     
+    void Start()
+    {
+        //allTiles = new BackgroundTile[width, height];
+        allDots = new GameObject[width, height];
+        Setup();
+    }
+
+    /// <summary>
+    /// Sets up the grid and instantiates the color in the grid box
+    /// </summary>
+    private void Setup()
+    {
+        for (int i = 0; i < width; i++)
+        {
+            for (int j = 0; j < height; j++)
+            {
+                // Grids
+                Vector2 gridPosition = new Vector2(i, j);
+                GameObject backGroundTile = Instantiate(tilePrefab, gridPosition, Quaternion.identity) as GameObject;
+                backGroundTile.transform.parent = this.transform;
+                backGroundTile.transform.name = $"( {i}, {j} )";
+
+                // Dots
+                int randomDots = Random.Range(0, dots.Length);
+
+                int maxIterations = 0;
+
+                // On game start, ensure that there are no 3 or more matching column or rows
+                while (FindMatchesAt(i, j, dots[randomDots]) && maxIterations < 100)
+                {
+                    randomDots = Random.Range(0, dots.Length);
+                    maxIterations++;
+                }
+                maxIterations = 0;
+
+                GameObject dotToInstantiate = Instantiate(dots[randomDots], gridPosition, Quaternion.identity) as GameObject;
+                dotToInstantiate.transform.parent = this.transform;
+                dotToInstantiate.name = $"( {i}, {j} )";
+
+                // Populate the 2D array
+                allDots[i, j] = dotToInstantiate;
+            }
+        }
+    }
+
+    /// <summary>
+    /// Return true if the game objects in the first and second column or row  have the same tag with gameobject
+    /// to be instantiated
+    /// </summary>
+    /// <param name="column">Represents the horizontal grid</param>
+    /// <param name="row">Represents the vertical grid</param>
+    /// <param name="currentDot">The game object to be instantiated</param>
+    /// <returns></returns>
+    private bool FindMatchesAt(int column, int row, GameObject currentDot)
+    {
+        if (column > 1 && row > 1)
+        {
+            if (allDots[column - 1, row].tag == currentDot.tag && allDots[column - 2, row].tag == currentDot.tag)
+            {
+                return true;
+            }
+            if (allDots[column, row - 1].tag == currentDot.tag && allDots[column, row - 2].tag == currentDot.tag)
+            {
+                return true;
+            }
+        }
+        else if (column <= 1 || row <= 1)
+        {
+            if (row > 1)
+            {
+                if (allDots[column, row - 1].tag == currentDot.tag && allDots[column, row - 2].tag == currentDot.tag)
+                {
+                    return true;
+                }
+            }
+            if (column > 1)
+            {
+                if (allDots[column - 1, row].tag == currentDot.tag && allDots[column - 2, row].tag == currentDot.tag)
+                {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    /// <summary>
+    /// If there is a match, destroy the gameObject at this position and set it to null
+    /// </summary>
+    /// <param name="column"></param>
+    /// <param name="row"></param>
+   private void DestroyMatchesAt(int column, int row)
+   {
+        if (allDots[column, row].GetComponent<DotTracker>().isMatched)
+        {
+            Destroy(allDots[column, row]);
+            allDots[column, row] = null;
+        }
+   }
+
+    /// <summary>
+    /// Loop through the board to find a gameObject to destroy
+    /// </summary>
+    public void DestroyMatches()
+    {
+        for (int i = 0; i < width; i++)
+        {
+            for (int j = 0; j < height; j++)
+            {
+                if (allDots[i, j] != null)
+                {
+                    DestroyMatchesAt(i, j);
+                }
+            }
+        }
+        StartCoroutine(DecreaseRows());
+    }
+
+    public IEnumerator DecreaseRows()
+    {
+        int nullCount = 0;
+        for (int i = 0; i < width; i++)
+        {
+            for (int j = 0; j < height; j++)
+            {
+                if (allDots[i, j] == null)
+                {
+                    nullCount++;
+                }
+                else if (nullCount > 0)
+                {
+                    allDots[i, j].GetComponent<DotTracker>().row -= nullCount;
+                    allDots[i, j] = null;
+                }
+            }
+            nullCount = 0;
+        }
+
+        yield return new WaitForSeconds(.4f);
+        StartCoroutine(FillBoard());
+    }
+
+    private void RefillBoard()
+    {
+        for (int i = 0; i < width; i++)
+        {
+            for (int j = 0; j < height; j++)
+            {
+                if (allDots[i, j] == null)
+                {
+                    Vector2 tempPosition = new Vector2(i, j);
+                    int random = Random.Range(0, dots.Length);
+                    GameObject instantiatedDot = Instantiate(dots[random], tempPosition, Quaternion.identity);
+                    allDots[i, j] = instantiatedDot;
+                }
+            }
+        }
+    }
+
+    private bool isDotsMatched()
+    {
+        for (int i = 0; i < width; i++)
+        {
+            for (int j = 0; j < height; j++)
+            {
+                if (allDots[i, j] != null)
+                {
+                    if (allDots[i, j].GetComponent<DotTracker>().isMatched)
+                    {
+                        return true;
+                    }
+                }
+            }
+        }
+
+        return false;
+    }
+
+    private IEnumerator FillBoard()
+    {
+        RefillBoard();
+        yield return new WaitForSeconds(.5f);
+        while (isDotsMatched())
+        {
+            yield return new WaitForSeconds(.5f);
+            DestroyMatches();
+        }
+        
+    }
+}
---
+++ Assets/Scripts/DotTracker.cs
@@ -0,0 +1,215 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DotTracker : MonoBehaviour
+{
+    private Vector2 firstTouchPosition;
+    private Vector2 finalTouchPosition;
+    private Vector2 tempPosition;
+
+    public int column;
+    public int row;
+    public int previousColumn;
+    public int previousRow;
+    public int targetX;
+    public int targetY;
+    private int swipeResist = 1;
+
+    public bool isMatched = false;
+
+    private Board board;
+
+    private GameObject otherDot;
+
+    public float swipeAngle = 0;
+    void Start()
+    {
+        targetX = (int)transform.position.x;
+        targetY = (int)transform.position.y;
+
+        column = targetX;
+        row = targetY;
+
+        previousColumn = column;
+        previousRow = row;
+
+        board = FindObjectOfType<Board>();
+    }
+    
+    void Update()
+    {
+        FindMatches(); 
+        if (isMatched)
+        {
+            SpriteRenderer sprite = GetComponent<SpriteRenderer>();
+            sprite.color = new Color(0, 0, 0, 2f);
+        }
+
+        targetX = column;
+        targetY = row;
+
+        if (Mathf.Abs(targetX - transform.position.x) > .1)
+        {
+            // Move towards the target in the horizontal direction
+            tempPosition = new Vector2(targetX, transform.position.y);
+            transform.position = Vector2.Lerp(transform.position, tempPosition, .2f);
+
+            if (board.allDots[column, row] != this.gameObject)
+            {
+                board.allDots[column, row] = this.gameObject;
+            }
+        }
+        else
+        {
+            // Directly set the position
+            tempPosition = new Vector2(targetX, transform.position.y);
+            transform.position = tempPosition;
+        }
+
+        if (Mathf.Abs(targetY - transform.position.y) > .1)
+        {
+            // Move towards the target in the vertical direction
+            tempPosition = new Vector2(transform.position.x, targetY);
+            transform.position = Vector2.Lerp(transform.position, tempPosition, .2f);
+
+            if (board.allDots[column, row] != this.gameObject)
+            {
+                board.allDots[column, row] = this.gameObject;
+            }
+        }
+        else
+        {
+            tempPosition = new Vector2(transform.position.x, targetY);
+            transform.position = tempPosition;
+        }
+    }
+
+    /// <summary>
+    /// This is called when a mouse button is pressed while over the GUI element or collider
+    /// </summary>
+    private void OnMouseDown()
+    {
+        firstTouchPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        print(firstTouchPosition);
+    }
+
+    /// <summary>
+    /// This is called when the mouse button is released
+    /// </summary>
+    private void OnMouseUp()
+    {
+        finalTouchPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        CalculateAngle();
+    }
+
+    private void CalculateAngle()
+    {
+        if (Mathf.Abs(finalTouchPosition.y - firstTouchPosition.y) > swipeResist || Mathf.Abs(finalTouchPosition.x - firstTouchPosition.x) > swipeResist)
+        {
+            swipeAngle = Mathf.Atan2(finalTouchPosition.y - firstTouchPosition.y, finalTouchPosition.x -
+            firstTouchPosition.x) * 180 / Mathf.PI;
+            print(swipeAngle);
+            MovePieces();
+        }
+    }
+
+    /// <summary>
+    /// Moves the Dots in up, right, down or left direction depending on the swipe angle
+    /// </summary>
+    private void MovePieces()
+    {
+        if (swipeAngle > -45 && swipeAngle <= 45 && column < board.width - 1)
+        {
+            // Right swipe
+            otherDot = board.allDots[column + 1, row];
+            otherDot.GetComponent<DotTracker>().column -= 1;
+            column += 1;
+        }
+        else if (swipeAngle > 45 && swipeAngle <= 135 && row < board.height - 1)
+        {
+            // Up swipe
+            otherDot = board.allDots[column, row + 1];
+            otherDot.GetComponent<DotTracker>().row -= 1;
+            row += 1;
+        }
+        else if ((swipeAngle > 135 || swipeAngle <= -135) && column > 0)
+        {
+            // Left swipe
+            otherDot = board.allDots[column - 1, row];
+            otherDot.GetComponent<DotTracker>().column += 1;
+            column -= 1;
+        }
+        else if (swipeAngle < -45 && swipeAngle >= -135 && row > 0)
+        {
+            // Down swipe
+            otherDot = board.allDots[column, row - 1];
+            otherDot.GetComponent<DotTracker>().row += 1;
+            row -= 1;
+        }
+
+        StartCoroutine(ResetPositionIfNoMatchIsDetected());
+    }
+
+    /// <summary>
+    /// Checks if the surrounding gameObjects have the same tag with THIS gameObject, 
+    /// and if it does, set isMatched to true.
+    /// </summary>
+    void FindMatches()
+    {
+        if (column > 0 && column < board.width - 1)
+        {
+            GameObject leftDot = board.allDots[column - 1, row];
+            GameObject rightDot = board.allDots[column + 1, row];
+
+            if (leftDot != null && rightDot != null)
+            {
+                if (leftDot.tag == this.gameObject.tag && rightDot.tag == this.gameObject.tag)
+                {
+                    leftDot.GetComponent<DotTracker>().isMatched = true;
+                    rightDot.GetComponent<DotTracker>().isMatched = true;
+                    isMatched = true;
+                }
+            }
+        }
+
+        if (row > 0 && row < board.height - 1)
+        {
+            GameObject upDot = board.allDots[column, row + 1];
+            GameObject downDot = board.allDots[column, row - 1];
+
+            if (upDot != null && downDot != null )
+            {
+                if (upDot.tag == gameObject.tag && downDot.tag == gameObject.tag)
+                {
+                    upDot.GetComponent<DotTracker>().isMatched = true;
+                    downDot.GetComponent<DotTracker>().isMatched = true;
+                    isMatched = true;
+                }
+            }
+        }
+    }
+
+    /// <summary>
+    /// If no match is detected between swiped Dots, reset their position to their previous position
+    /// </summary>
+    /// <returns></returns>
+    public IEnumerator ResetPositionIfNoMatchIsDetected()
+    {
+        yield return new WaitForSeconds(.5f);
+
+        if (otherDot != null)
+        {
+            if (!isMatched && !otherDot.GetComponent<DotTracker>().isMatched)
+            {
+                otherDot.GetComponent<DotTracker>().row = row;
+                otherDot.GetComponent<DotTracker>().column = column;
+
+                row = previousRow;
+                column = previousColumn;
+            }
+                        board.DestroyMatches();
+            otherDot = null;
+        }
+    }
+}
